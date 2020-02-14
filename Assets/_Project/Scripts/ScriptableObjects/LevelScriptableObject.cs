349ab3ee812d4b62dcc44b5abd8cbda47a8b8a14
Ismael Nascimento 'Ash <contato@ismaelnascimento.com>
2020-02-13 23:52:23 -0300

    [ADDED] ScriptableObjets of levels [UPDATED] Path of screenshots for root project [UPDATED] StartMenu scene, user interface

---
+++ Assets/_Project/Scripts/ScriptableObjects/LevelScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "BlockBreaker/LevelData")]
+public class LevelScriptableObject : ScriptableObject
+{
+    public new string name;
+    public Sprite background;
+    public Color color;
+    public GameObject blocksPrefab;
+}
